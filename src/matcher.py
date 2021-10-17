44a53f56fbc3cfb157684265694ccf44c5e34553
sandbornm <michael.sandborn@vanderbilt.edu>
2021-10-17 17:36:36 -0500

    interactive plots, added metrics, HD wip

--- src/matcher.py
+++ src/matcher.py
@@ -1,62 +1,73 @@
 import streamlit as st
 import os
-from utils import compare
+from utils import compare, generate, plotly
 import timeit
+import pandas as pd
 
 st.set_page_config(layout="wide")
 st.header("Impedance Matcher")
 
-# select or upload a file
-
-
 upload = st.checkbox("Upload files")
+generate = st.checkbox("Generate synthetic data")
 
-if upload:
-    base = st.file_uploader("Upload a baseline signature", type=["csv"])
-    sub = st.file_uploader("Upload a subsequent signature", type=["csv"])
+if generate:
+    mu = st.slider("mean for noise", 0, 10, 0)
+    sigma = st.slider("std dev for noise", 0.0, 1.0, 0.05)
+    samples = st.slider("number of samples", 10, 1000, 100)
+    
+    if st.button("Generate"):
+        st.write(mu)
+        st.write(sigma)
+        st.write(samples)
+elif upload:
+    base = st.file_uploader("Upload a baseline measurement", type=["csv"])
+    sub = st.file_uploader("Upload a subsequent measurement", type=["csv"])
+    assert base is not None
+    assert sub is not None
+
 else:
     dataDir = os.path.join(os.path.dirname(os.getcwd()), "data")
-    base = st.selectbox("Select a baseline measurement", os.listdir(dataDir))
-    sub = st.selectbox("Select a subsequent measurement", os.listdir(dataDir))
+    files = [f for f in os.listdir(dataDir) if f[-4:] == ".csv"]
+    base = st.selectbox("Select a baseline measurement", files)
+    sub = st.selectbox("Select a subsequent measurement", files)
 
 if base and sub and st.button("Compare"):
-    # do comparison
     with st.spinner("comparing..."):
         s = timeit.timeit()
-        pb, ps, pt, pa, ib, iu, it, ia, basePks, baseProm, subPks, subProm = compare(base, sub)
+        pl = plotly(base, sub)
+        cmp, base, sub = compare(base, sub)
         e = timeit.timeit()
     st.write(f"done in {s-e}s")
-    if pt == pa:
-        st.success("Peak Match")
-        st.write(f"peak base data: {pb}")
-        st.write(f"peak sub data: {ps}")
-        st.write(f"peak target key: {pt}")
-        st.write(f"peak actual key: {pa}")
-        st.write(f"peak base raw : {basePks}") # frequencies
-        st.write(f"peak base prom : {baseProm}") # prominences
-        st.write(f"peak sub raw : {subPks}") # frequencies
-        st.write(f"peak sub prom : {subProm}") # prominences
+
+    c1, c2, c3 = st.beta_columns((1, 1, 2))
+    c3.plotly_chart(pl)
+
+    c1.write(f"peak base raw : {cmp.base.peakFrq}") # frequencies
+    c1.write(f"peak base prom : {cmp.base.prom}") # prominences
+    c1.write(f"peak sub raw : {cmp.sub.peakFrq}") # frequencies
+    c1.write(f"peak sub prom : {cmp.sub.prom}") # prominences
+    c1.write(f"peak base data: {cmp.peakBase}")
+    c1.write(f"peak sub data: {cmp.peakSub}")
+    c1.write(f"peak target key: {cmp.peakTargetKey}")
+    c1.write(f"peak actual key: {cmp.peakActualKey}")
+
+    if cmp.peakTargetKey == cmp.peakActualKey:
+        c1.success("Peak Match")
     else:
-        st.error("Peak mismatch")
-        st.write(f"peak base data: {pb}")
-        st.write(f"peak sub data: {ps}")
-        st.write(f"peak target key: {pt}")
-        st.write(f"peak actual key: {pa}")
-        st.write(f"peak base raw : {basePks}") # frequencies
-        st.write(f"peak base prom : {baseProm}") # prominences
-        st.write(f"peak sub raw : {subPks}") # frequencies
-        st.write(f"peak sub prom : {subProm}") # prominences
-    if it == ia:
-        st.success("Impedance Match")
-        st.write(f"impedance base data: {ib}")
-        st.write(f"impedance sub data: {iu}")
-        st.write(f"impedance target key: {it}")
-        st.write(f"impedance actual key: {ia}")
+        c1.error("Peak mismatch")
+
+    c2.write(f"imp base data: {cmp.impBase}")
+    c2.write(f"imp sub data: {cmp.impSub}")
+    c2.write(f"imp target key: {cmp.impTargetKey}")
+    c2.write(f"imp actual key: {cmp.impActualKey}")
+        
+    if cmp.impTargetKey == cmp.impActualKey:
+        c2.success("Impedance Match")
     else:
-        st.error("Impedance mismatch")
-        st.write(f"impedance base data: {ib}")
-        st.write(f"impedance sub data: {iu}")
-        st.write(f"peak target key: {it}")
-        st.write(f"peak actual key: {ia}")
+        c2.error("Impedance mismatch")
+
+    for k, v in cmp.metrics.items():
+        st.write(k, v) 
+        
 
     
--- src/utils.py
+++ src/utils.py
@@ -1,44 +1,67 @@
 import os
 import pandas as pd
 import numpy as np
+from pandas.core import base
 from scipy.signal import find_peaks
 from scipy.signal.ltisys import impulse
 from fuzzyExtractor import FuzzyExtractor
+from sklearn.metrics import mean_squared_error
+import plotly.graph_objects as go
+
+
+class DataGenerator():
+    def __init__(self, mu=0, sigma=0.05):
+        self.mu = mu
+        self.sigma = sigma
+    
+    def generate(self, lo=5000, hi=100000, step=20):
+        self.freqRange = np.arange(lo, hi+step, step)
+        self.size = len(self.freqRange)
+
+    # todo generate data according to desired shape?
+    # todo gaussian vs non-gaussian noise
 
 
 class Part:
-    def __init__(self, filename, length=5, tolerance=5):
-        assert filename[-4:] == ".csv", "CSV file expected" 
-        self.filename = filename
-        self.loadData()
+    def __init__(self, file, length=5, tolerance=5):
+        if isinstance(file, str):
+            assert file[-4:] == ".csv", "CSV file expected" 
+            self.filename = file
+            self.loadData()
+        else:
+            file.seek(0) # weird hack to set file position for pandas to detect data
+            self.loadData(upload=True, data=file) 
+        
         self.getPeaks()
+        self.getBinary()
         self.normData()
         self.length = length
         self.tolerance = tolerance
-        # print("peaks")
-        # print(self.peaks)
-
         self.peakBytes = peaksToByteArray(self.peaks)
         self.impBytes = self.packImpedance()
 
-        # for peaks
+        # peaks
         print("Fe peaks")
         self.fe = FuzzyExtractor(length, tolerance)
         self.k, self.h = self.fe.generate(self.peakBytes[:length])
 
-        # for impedance
+        # impedance
         print("Fe impedance")
         self.fei = FuzzyExtractor(length, tolerance)
         self.ki, self.hi = self.fe.generate(self.impBytes[:length])
+
         
-    def loadData(self):
-        df = pd.read_csv(os.path.join(os.path.dirname(os.getcwd()), "data", self.filename), header=None)
+        
+    def loadData(self, upload=False, data=None):
+        if upload:
+            df = pd.read_csv(data, header=None) # pandas reads file as buffer
+        else:
+            df = pd.read_csv(os.path.join(os.path.dirname(os.getcwd()), "data", self.filename), header=None)
+        
+        assert len(df.columns) == 3, "bad file format, expected 3 columns"
         self.freq = np.array(df.loc[:, 0])
         self.realImp = np.array(df.loc[:, 1])
         self.imagImp = np.array(df.loc[:, 2])
-        # print(self.freq[:10])
-        # print(self.realImp[:10])
-        # print(self.imagImp[:10])
 
     def __repr__(self):
         s = f"Name: {self.filename.split('/')[-1]}\n"
@@ -47,9 +70,6 @@ class Part:
         return s
 
     def getPeaks(self, numPeaks=10):
-        # todo sort and select n most prominent
-        # peak properties has prominence, sort by prominence
-        # 
         pks, props = find_peaks(self.realImp, prominence=1)
         peakIdx = np.flip(np.argsort(props['prominences']))[:numPeaks] # this is sorted peak prominence in descending order
         self.peaks = pks[peakIdx]
@@ -70,9 +90,32 @@ class Part:
         print("packed impedance")
         print(bytes)
         return bytes
-        #return b''.join(bytearray(byte) for byte in bytes)
-    
-        
+
+    """ make the impedance data into a binary string by taking the first digit past the decimal
+    of the normalized impedance values and converting to binary"""
+    def getBinary(self):
+        bmap = {0: "0000",
+                1: "0001",
+                2: "0010",
+                3: "0011",
+                4: "0100",
+                5: "0101",
+                6: "0110",
+                7: "0111",
+                8: "1000",
+                9: "1001",
+                }
+        s = ""
+        for x in self.normReal:
+            if x == 1:
+                s+= bmap[1]
+            else: 
+                y = x * 10
+                z = int(y)
+                s+= bmap[z]
+        b = bin(int(s, 2))
+        self.binary = b # this is the binary form of all tenths place normalized impedance values
+
 
 """ compare 2 parts by specifying a baseline and retrieving
     fuzzy extractor data
@@ -94,13 +137,6 @@ class Comparator:
         self.peakTargetKey, self.peakHelpers = self.base.k, self.base.h
         self.peakActualKey = self.base.fe.reproduce(self.peakSub, self.peakHelpers)
 
-
-        # print("Peaks")
-        # print(f"source: {self.peakSource}")
-        # print(f"input: {}")
-        # print(f"Baseline key: {self.peakTarget}")
-        # print(f"Subsequent key: {self.peakActual}")
-
         # keep normalized impedance byte data 
         self.impBase = self.base.impBytes[:self.base.tolerance]
         self.impSub = self.sub.impBytes[:self.base.tolerance]
@@ -108,12 +144,33 @@ class Comparator:
         # keys for impedance
         self.impTargetKey, self.impHelpers = self.base.ki, self.base.hi
         self.impActualKey = self.base.fei.reproduce(self.impSub, self.impHelpers)
-            
-        # print("Impedance")
-        # print(f"source: {base.impBytes[:base.tolerance]}")
-        # print(f"input: {sub.impBytes[:base.tolerance]}")
-        # print(f"Baseline key: {tar}")
-        # print(f"Subsequent key: {act}")
+
+        # get metrics
+        self.metrics = self.getMetrics()
+
+    def getHammingDistance(self):
+        baseBin = self.base.binary
+        subBin = self.sub.binary
+        assert len(baseBin) == len(subBin), "length mismatch"
+
+        cnt = 0
+        for i in range(len(baseBin)):
+            if baseBin[i] != subBin[i]:
+                cnt += 1
+        return cnt
+
+    def getMetrics(self):
+        names = ["mse", "rmse", "l1", "hamming"] # add others
+        mse = mean_squared_error(self.base.normReal, self.sub.normReal)
+        rmse = np.sqrt(mse)
+        l1 = self.base.normReal - self.sub.normReal
+        ham = self.getHammingDistance()
+
+        return dict(zip(names, [mse, rmse, l1, ham]))   
+
+
+
+""" Standalone helpers """
 
 
 def peaksToByteArray(peaks):
@@ -128,17 +185,37 @@ def compare(base, sub):
     # todo add viz - 
     # todo perturb the baseline to "damage" change every 100 values by some noise
     # todo check byte symbols in python peak sub data: b'\x00\x13\x00F\x00' from AD2_DitherA5.csv
-    # todo add binary strings corresponding
 
-    return (cmp.peakBase,
-            cmp.peakSub,
-            cmp.peakTargetKey,
-            cmp.peakActualKey,
-            cmp.impBase,
-            cmp.impSub,
-            cmp.impTargetKey, 
-            cmp.impActualKey,
-            cmp.base.peakFrq,
-            cmp.base.prom, 
-            cmp.sub.peakFrq,
-            cmp.sub.prom)
+    return cmp, basePart, subPart
+
+""" make interactive plotly charts instead of matplotlib static charts"""
+def plotly(base, sub):
+    basePart = Part(base)
+    subPart = Part(sub)
+
+    fig = go.Figure({'layout': {'title' : {'text' : 'Base vs. Sub (normalized)'} } })
+    
+    # base
+    fig.add_trace(go.Scatter(
+        x=basePart.freq,
+        y=basePart.normReal,
+        mode="lines", 
+        name="base signature"
+    ))
+
+    # sub
+    fig.add_trace(go.Scatter(
+        x=subPart.freq,
+        y=subPart.normReal,
+        mode="lines", 
+        name="sub signature"
+    ))
+
+    return fig
+
+
+""" create a data generator based on selected mu and sigma
+    for the noise distribution and generate samples number 
+    of data points (1 sample is a single impedance signature)"""
+def generate(mu, sigma, samples):
+    pass
